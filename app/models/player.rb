8ebff7cb636ecec35d7fc8e906ac24df1cf03c25
PabloSKZ <pablo.eskenazi@gmail.com>
2020-06-16 11:49:39 +0200

    migration attribute + model dependencies (commented) + JTW-devise in model

---
+++ app/models/player.rb
@@ -0,0 +1,13 @@
+class Player < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JwtBlacklist
+  
+  # belongs_to :team
+  # has_many :emergency_contacts
+  # has_many :games, through: :event
+  # has_many :practices, through: :event
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_083040) do
+ActiveRecord::Schema.define(version: 2020_06_15_220314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,17 +18,17 @@ ActiveRecord::Schema.define(version: 2020_06_16_083040) do
   create_table "coaches", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
     t.string "phone"
     t.date "birthdate"
     t.date "arrival"
     t.boolean "admin?"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_coaches_on_email", unique: true
     t.index ["reset_password_token"], name: "index_coaches_on_reset_password_token", unique: true
   end
