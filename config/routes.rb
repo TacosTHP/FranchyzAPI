696cac46a9e94e4859807d4c37213ecbaa30028c
Dim <dimitri.kiavue@gmail.com>
2020-06-16 16:21:45 +0200

    [UPDATE] Nested Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-
-  resources :practices
-  resources :emergency_contacts
-
-
-  resources :teams
-
-  resources :clubs
-  resources :games
   devise_for :players
-
-
   devise_for :coaches
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :coaches
+  resources :clubs do
+    resources :teams do
+      resources :practices
+      resources :games
+      resources :players do
+        resources :emergency_contacts
+      end
+    end
+  end
 
-    # => ADDITIONNAL GAMES ROUTES
-    get '/mygames',            to: 'games#mygames'
+  # => ADDITIONNAL GAMES ROUTES
+  get '/mygames',            to: 'games#mygames'
 end
