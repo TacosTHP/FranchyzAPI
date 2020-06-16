74d093d50e5d484d28f0c2f3f1591365c17a5d38
carolin.labinski <carolin.labinski@yahoo.de>
2020-06-16 13:09:53 +0200

    solve routes.rb conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :games
-=======
   devise_for :players
->>>>>>> 5907a25935d3f7d314218d22afa0f04c8cf535ef
   devise_for :coaches
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
