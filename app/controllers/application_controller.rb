18823b23cee97db0468b9cdc2211998403e9fec6
PabloSKZ <pablo.eskenazi@gmail.com>
2020-06-16 17:33:44 +0200

    auth devise params in sign up : first_name, last name, birthdate, phone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
-  include UsersHelper
+  # include UsersHelper
 
   respond_to :json
 
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password)}
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password, :first_name, :last_name, :phone, :birthdate, :gender)}
     devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:email, :password)}
   end
 end
