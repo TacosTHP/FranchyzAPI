1d216e37a2e696b3c36f7ac68768335886e5f6a4
Dim <dimitri.kiavue@gmail.com>
2020-06-16 17:02:18 +0200

    [ADD] Link between Game, Player and Event

--- app/models/player.rb
+++ app/models/player.rb
@@ -8,6 +8,6 @@ class Player < ApplicationRecord
 
   belongs_to :team
   has_many :emergency_contacts
-  # has_many :games, through: :event
+  has_many :games, through: :event
   # has_many :practices, through: :event
 end
--- db/migrate/20200616083103_create_games.rb
+++ db/migrate/20200616083103_create_games.rb
@@ -6,13 +6,12 @@ class CreateGames < ActiveRecord::Migration[6.0]
       t.string :street
       t.string :street_number
       t.string :country
-      t.string :zip_code    
+      t.string :zip_code
       t.datetime :starting_date_time
       t.integer :duration
       t.integer :home_team_score
       t.integer :away_team_score
       t.boolean :canceled
-
       t.timestamps
     end
   end
