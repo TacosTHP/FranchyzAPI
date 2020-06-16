2a30964c6f51d5dc99693b73720ff893e028276f
Dim <dimitri.kiavue@gmail.com>
2020-06-16 16:00:49 +0200

    [ADD] Link between Team and Club

--- db/migrate/20200616091347_create_teams.rb
+++ db/migrate/20200616091347_create_teams.rb
@@ -4,7 +4,7 @@ class CreateTeams < ActiveRecord::Migration[6.0]
       t.string      :title
       t.belongs_to  :coach
       t.belongs_to  :creator, index: true, foreign_key: {to_table: :coaches}
-      # t.belongs_to  :club
+      t.belongs_to  :club
       t.timestamps
     end
   end
