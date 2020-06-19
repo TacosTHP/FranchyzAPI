1a70be541688d2bca2d1ea5903eb0e55944fcefe
PabloSKZ <pablo.eskenazi@gmail.com>
2020-06-19 15:04:47 +0200

    url login prod

--- app/mailers/coach_mailer.rb
+++ app/mailers/coach_mailer.rb
@@ -6,7 +6,7 @@ class CoachMailer < ApplicationMailer
     @coach = coach 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'https://github.com/TacosTHP/Franchyz' 
+    @url  = 'https://franchyz-prod.herokuapp.com/login' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @coach.email, subject: 'Bienvenue chez Franchyz !') 
--- app/mailers/player_mailer.rb
+++ app/mailers/player_mailer.rb
@@ -6,7 +6,7 @@ class PlayerMailer < ApplicationMailer
     @player = player 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'https://github.com/TacosTHP/Franchyz' 
+    @url  = 'https://franchyz-prod.herokuapp.com/login' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @player.email, subject: 'Bienvenue chez Franchyz !') 
