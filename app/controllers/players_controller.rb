512656a30eecf4d1da3ad53163739bcd76609426
mariannehbt <marianne.heribert@gmail.com>
2020-06-19 17:34:25 +0200

    add before_action to controller players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,7 @@
 class PlayersController < ApplicationController
+	before_action :check_auth, only: [:show, :create, :edit, :update, :delete]
+	before_action :decode_token, only: [:create, :edit, :update, :delete]
+
 	def show
 		@player = Player.find(params[:id])
 		render json: @player
