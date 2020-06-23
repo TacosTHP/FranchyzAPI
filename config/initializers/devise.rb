67fc7339ab968a9d62e171edb3adf6151477235b
PabloSKZ <pablo.eskenazi@gmail.com>
2020-06-23 14:57:34 +0200

    modif PATCH to PUT

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -318,7 +318,8 @@ Devise.setup do |config|
       ['POST', %r{^/players/sign_in.json$}],
       ['POST', %r{^/players.json$}],
       ['POST', %r{^/clubs.json$}],
-      ['PATCH', %r{^/players/\d+\.json$}]
+      ['PATCH', %r{^/players/\d+\.json$}],
+      ['PUT', %r{^/players/\d+\.json$}]
     ]
 
     jwt.revocation_requests = [
