5903a6a865819188fc542c6f9cce89203963440f
mariannehbt <marianne.heribert@gmail.com>
2020-06-25 15:34:09 +0200

    test new tab_size 3

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,30 +1,29 @@
 class CoachesController < ApplicationController
-	before_action :check_auth, only: [:show, :create, :edit, :update, :delete]
-	before_action :decode_token, only: [:create, :edit, :update, :delete]
-	# before_action :check_admin, only: [:create, :edit, :update, :delete]
-	before_action :set_coach, only: [:show, :update, :destroy]
-	before_action :authenticate_coach!, only: [:update]
+  before_action :check_auth, only: [:show, :create, :edit, :update, :delete]
+  before_action :decode_token, only: [:create, :edit, :update, :delete]
+  # before_action :check_admin, only: [:create, :edit, :update, :delete]
+  before_action :set_coach, only: [:show, :update, :destroy]
+  before_action :authenticate_coach!, only: [:update]
 
-	def show
-		@coach = Coach.find(params[:id])
-		render json: @coach
-	end
+  def show
+    @coach = Coach.find(params[:id])
+    render json: @coach
+  end
 
-	def update
-		if @coach.update(coach_params)
-			render json: @coach
-		else
-			render json: @coach.errors, status: :unprocessable_entity
-		end
-	end
+  def update
+    if @coach.update(coach_params)
+      render json: @coach
+    else
+      render json: @coach.errors, status: :unprocessable_entity
+    end
+  end
 
-	private
-	def set_coach
-		@coach = Coach.find(params[:id])
-	end
+  private
+  def set_coach
+    @coach = Coach.find(params[:id])
+  end
 
-	def coach_params
-		params.require(:coach).permit(:first_name, :last_name, :phone, :birthdate, :arrival, :admin?, :club_id)
-	end
-	
+  def coach_params
+    params.require(:coach).permit(:first_name, :last_name, :phone, :birthdate, :arrival, :admin?, :club_id)
+  end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,27 +1,26 @@
 class PlayersController < ApplicationController
-	before_action :authenticate_player!, only: [:update]
-	before_action :set_player, only: [:show, :update, :destroy]
+  before_action :authenticate_player!, only: [:update]
+  before_action :set_player, only: [:show, :update, :destroy]
 
-	def show
-		@player = Player.find(params[:id])
-		render json: @player
-	end
+  def show
+    @player = Player.find(params[:id])
+    render json: @player
+  end
 
-	def update
-		if @player.update(player_params)
-			render json: @player
-		else
-			render json: @player.errors, status: :unprocessable_entity
-		end
-	end
+  def update
+    if @player.update(player_params)
+      render json: @player
+    else
+      render json: @player.errors, status: :unprocessable_entity
+    end
+  end
 
-	private
-	def set_player
-		@player = Player.find(params[:id])
-	end
+  private
+  def set_player
+    @player = Player.find(params[:id])
+  end
 
-	def player_params
-		params.require(:player).permit(:first_name, :last_name, :phone, :birthdate, :arrival, :availability?, :height, :weight, :gender, :jersey_number, :position, :team_id)
-	end
-	
+  def player_params
+    params.require(:player).permit(:first_name, :last_name, :phone, :birthdate, :arrival, :availability?, :height, :weight, :gender, :jersey_number, :position, :team_id)
+  end
 end
