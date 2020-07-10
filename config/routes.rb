60465c5a2c51704df50acc921c4a07ab44640a82
floury-fabio <floury-fabio@hotmail.fr>
2020-07-10 13:18:21 +0200

    refactor_routes done

--- config/routes.rb
+++ config/routes.rb
@@ -2,28 +2,15 @@ Rails.application.routes.draw do
   devise_for :players
   devise_for :coaches
 
-  resources :players, except:[:new, :create, :delete]
-  resources :coaches, except:[:new, :create, :delete]
+  resources :coaches, except: [:new, :create, :destroy]
+  resources :attendances
+  resources :practices
+  resources :games
   resources :clubs do
     resources :teams do
-      resources :practices
-      resources :games
-      resources :players do
-        resources :events
+      resources :players, except: [:new, :create, :destroy] do
         resources :emergency_contacts
       end
     end
   end
-
-    # => ADDITIONNAL CLUB ROUTES
-  get '/clubs/:id/admin',            to: 'club#dashboard_admin'
-
-  # => ADDITIONNAL EVENTS ROUTES
-  get '/clubs/:club_id/teams/:team_id/players/:player_id/unconfirmed_events', to: 'events#unconfirmed_events'
-  get '/clubs/:club_id/teams/:team_id/players/:player_id/my_attended_games', to: 'events#my_attended_games'
-  get '/clubs/:club_id/teams/:team_id/players/:player_id/my_unattended_games', to: 'events#my_unattended_games'
-  get '/clubs/:club_id/teams/:team_id/players/:player_id/my_attended_practices', to: 'events#my_attended_practices'
-  get '/clubs/:club_id/teams/:team_id/players/:player_id/my_unattended_practices', to: 'events#my_unattended_practices'
-  get '/practices/:id',            to: 'practices#show'
-
 end
