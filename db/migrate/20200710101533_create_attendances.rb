a5eb3f837f986c8d7a59ca0ad3d46f35879f525c
floury-fabio <floury-fabio@hotmail.fr>
2020-07-10 12:18:15 +0200

    setup migrate file

--- db/migrate/20200710101533_create_attendances.rb
+++ db/migrate/20200710101533_create_attendances.rb
@@ -1,7 +1,10 @@
 class CreateAttendances < ActiveRecord::Migration[6.0]
   def change
     create_table :attendances do |t|
-
+      t.belongs_to  :player
+      t.belongs_to  :game
+      t.belongs_to  :practice
+      t.boolean :confirmed?, default: false
       t.timestamps
     end
   end
