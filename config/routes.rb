b63d2a6c32dd244533b3b5eff398f611c119854b
floury-fabio <floury-fabio@hotmail.fr>
2020-07-23 13:20:50 +0200

    versionning route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :players
-  devise_for :coaches
 
-  resources :coaches, except: [:new, :create, :destroy]
-  resources :attendances
-  resources :practices
-  resources :games
-  resources :clubs do
-    resources :teams do
-      resources :players, except: [:new, :create, :destroy] do
-        resources :emergency_contacts
+  namespace :api do
+    namespace :v1 do
+      devise_for :players
+      devise_for :coaches
+      resources :coaches, except: [:new, :create, :destroy]
+      resources :attendances
+      resources :practices
+      resources :games
+      resources :clubs do
+        resources :teams do
+          resources :players, except: [:new, :create, :destroy] do
+            resources :emergency_contacts
+          end
+        end
       end
     end
   end
